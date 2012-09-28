1969a8085795e7a1d1b62530e7f7b94d0dca869d
Jorge Morando <jmorando@cuyum.com>
2012-09-28 10:19:09 -0300

    Modified files for mongo and hornet

--- src/main/java/com/leafnoise/pathfinder/model/Message.java
+++ src/main/java/com/leafnoise/pathfinder/model/Message.java
@@ -2,38 +2,15 @@ package com.leafnoise.pathfinder.model;
 
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-
-
-@Entity
-@Table(name="Message")
 public class Message {
 
-	@Id
-	@GeneratedValue (strategy = GenerationType.SEQUENCE)
 	private Long id;
 	
-	@NotNull(message="Debe ingresar una fecha.")
-	@Past(message="La fecha no puede ser posterior a la actual")
 	private Date receivedDate;
 	
-	@NotNull(message="Debe tener un emisor.")
 	private String origin;
-	
 		
-	@NotNull(message="Debe tener un contenido.")
 	private String payload;
-	
-	
-	
-		
-	
 	
 	public Long getId() {
 		return id;
